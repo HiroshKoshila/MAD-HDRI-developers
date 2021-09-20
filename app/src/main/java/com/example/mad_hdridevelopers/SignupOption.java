e148817e15b0348c42f5a7f0840ca12be6812cdc
DinithiPallawala <87613484+DinithiPallawala@users.noreply.github.com>
2021-09-19 23:58:29 -0700

    business registration page

--- app/src/main/java/com/example/mad_hdridevelopers/SignupOption.java
+++ app/src/main/java/com/example/mad_hdridevelopers/SignupOption.java
@@ -2,7 +2,10 @@ package com.example.mad_hdridevelopers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupOption extends AppCompatActivity {
 
@@ -12,6 +15,16 @@ public class SignupOption extends AppCompatActivity {
         setContentView(R.layout.activity_signup_option);
 
         getSupportActionBar().hide();
-
+        Button wel2btn5=findViewById(R.id.wel2btn5);
+        wel2btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeActivity();
+            }
+        });
+    }
+    private void changeActivity(){
+        Intent business = new Intent(this,BusinessRegistration.class);
+        startActivity(business);
     }
 }
