11f4f1c13fe0b11f12ea4a3f45d1de53e0a12d44
ishmaSenaweera <86140992+ishmaSenaweera@users.noreply.github.com>
2021-09-24 13:43:13 +0530

    Bottom Navigationbar Added

--- app/src/main/java/com/example/mad_hdridevelopers/Photos.java
+++ app/src/main/java/com/example/mad_hdridevelopers/Photos.java
@@ -9,17 +9,16 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home extends AppCompatActivity {
-
+public class Photos extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_photos);
 
         bottomNavigationView = findViewById(R.id.bottom_navigator);
-        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.photos);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -30,16 +29,18 @@ public class Home extends AppCompatActivity {
                         return true;
 
                     case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), Home.class));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.photos:
-                        startActivity(new Intent(getApplicationContext(), Photos.class));
-                        overridePendingTransition(0,0);
                         return true;
 
                 }
                 return false;
             }
         });
+
+
     }
 }
