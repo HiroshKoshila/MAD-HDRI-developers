f6fd4390dc4a6a3b78e683d27074477ec0b7a06f
Hirosh Koshila <hiroshkoshila7@gmail.com>
2021-09-25 09:20:46 -0500

    Map Current Location part added successfully!

--- app/src/main/java/com/example/mad_hdridevelopers/CurrentLocation.java
+++ app/src/main/java/com/example/mad_hdridevelopers/CurrentLocation.java
@@ -59,8 +59,7 @@ public class CurrentLocation extends AppCompatActivity {
     private void getCurrentLocation() {
         //initialize task location
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
+
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
             //                                          int[] grantResults)
